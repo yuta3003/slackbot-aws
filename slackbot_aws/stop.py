e1b063fa848db692dafcaee06bd90259289f4bca
yuta3003 <y.nakazawa3003+github@gmail.com>
2023-06-30 23:01:37 +0900

    :+1: add type hint

--- slackbot_aws/stop.py
+++ slackbot_aws/stop.py
@@ -9,7 +9,7 @@ with open("credentials.json", "r", encoding="utf-8") as credentials:
     json = json.load(credentials)
 
 
-def stop_ec2(ec2_machines):
+def stop_ec2(ec2_machines: list):
     """ stop ec2 """
     if ec2_machines is None or len(ec2_machines) == 0:
         return
