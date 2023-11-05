1da0983af51b77baedd1e279b60cd9bd5d1810cb
yuta3003 <y.nakazawa3003+github@gmail.com>
2023-11-05 17:17:13 +0900

    :bug: バグ修正

--- slackbot_aws/getip.py
+++ slackbot_aws/getip.py
@@ -19,15 +19,10 @@ def get_ip(ec2_machines: list, instance):
     instance_list = instance.split()
     instance_id = instance_list[0]
     region_name = instance_list[1]
-    if ec2_machines is None or len(ec2_machines) == 0:
-        return
-    for machine in ec2_machines:
-        ec2 = aws.EC2(
-            instance_id=machine["InstanceId"], region_name=machine["RegionName"]
-        )
-        ec2.get_ec2_ip(
-            instance_id=instance_id, region_name=region_name
-        )
+    ec2 = aws.EC2()
+    ec2.get_ec2_ip(
+        instance_id=instance_id, region_name=region_name
+    )
 
 
 if __name__ == "__main__":
