2909ddbe3a499a04dab743ca614ea28fd3c489ba
yuta3003 <y.nakazawa3003+github@gmail.com>
2023-11-07 11:43:31 +0900

    :+1: update

--- slackbot_aws/fetch.py
+++ slackbot_aws/fetch.py
@@ -9,16 +9,11 @@ with open("credentials.json", "r", encoding="utf-8") as credentials:
     credential: dict[str, list] = json.load(credentials)
 
 
-def start_ec2(ec2_machines: list):
+def fetch_ec2(ec2_machines: list):
     """start"""
-    if ec2_machines is None or len(ec2_machines) == 0:
-        return
-    for machine in ec2_machines:
-        ec2 = aws.EC2(
-            instance_id=machine["InstanceId"], region_name=machine["RegionName"]
-        )
-        ec2.fetch_ec2_info()
+    ec2 = aws.EC2()
+    ec2.fetch_ec2_info()
 
 
 if __name__ == "__main__":
-    start_ec2(credential["EC2"])
+    fetch_ec2(credential["EC2"])
