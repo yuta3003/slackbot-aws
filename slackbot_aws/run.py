5db9bc433dbfac56fe9b3f1693f1129b871f3ddc
yuta3003 <y.nakazawa3003+github@gmail.com>
2023-11-07 11:40:46 +0900

    :+1: update

--- slackbot_aws/run.py
+++ slackbot_aws/run.py
@@ -9,15 +9,15 @@ with open("credentials.json", "r", encoding="utf-8") as credentials:
     credential: dict[str, list] = json.load(credentials)
 
 
-def start_ec2(ec2_machines: list):
+def start_ec2(ec2_machines: list, instance):
     """start"""
-    if ec2_machines is None or len(ec2_machines) == 0:
-        return
-    for machine in ec2_machines:
-        ec2 = aws.EC2(
-            instance_id=machine["InstanceId"], region_name=machine["RegionName"]
-        )
-        ec2.start()
+    instance_list = instance.split()
+    instance_id = instance_list[0]
+    region_name = instance_list[1]
+    ec2 = aws.EC2()
+    ec2.start(
+        instance_id=instance_id, region_name=region_name
+    )
 
 
 if __name__ == "__main__":
